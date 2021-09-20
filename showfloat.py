e2e3d2f2d072399cf1f67dc48d01e408529802d2
Greg Kronmiller <kronmillerg@gmail.com>
2021-09-19 23:25:48 -0700

    Decimal formatting

--- showfloat.py
+++ showfloat.py
@@ -1,11 +1,18 @@
 #import argparse
 import bigfloat
+import math
 
 def main():
     with mkContext(BINARY32):
         value = bigfloat.BigFloat.fromhex("-0x1.55554p-126")
         s, e, m = splitSEM(value, BINARY32)
         print "{:01b} {:08b} {:023b}".format(s, e, m)
+
+        print("")
+        showFloat(value, (s, e, m), BINARY32)
+
+        print("")
+        showFloat(value, (s, e, m), BINARY32, exactDecimal=True)
 
     # TODO actual impl
     pass
@@ -15,6 +22,61 @@ def selfTest():
     assert mkContext(BINARY64) == bigfloat.double_precision
 
 
+
+def showFloat(value, sem, fltFormat, exactDecimal=False):
+    """
+    Example format (tentative):
+
+    Dec (approx): 1.234
+    Hex (%a):     0x1.234p+0
+    10exp2:       1234 * 2**-12
+    ULP:          2 ** -12
+    FP_CLASSIFY:  FP_NORMAL
+    Bits (hex):   0x3fe12300
+    Bits (bin):   0 01111111 00010010001101000000000
+    """
+
+    # Decimal value
+    if exactDecimal:
+        # I think a longest exact base-10 representation for a floating-point
+        # format is:
+        #     nextDown(2*FLT_MIN)
+        #   = 2*FLT_MIN - MIN_FLT_SUBNORM
+        #   = (2**(1 + usefulMantBits) - 1) * 2**log2OfMinSubnorm
+        # whose decimal digits are the same as:
+        #     (2**(1 + usefulMantBits) - 1) * 5**-log2OfMinSubnorm
+        # In case I'm wrong, format it with twice that many digits and then
+        # assert that the number of sig figs is within my bound.
+        mostDigits = len(str((2**(1 + fltFormat.usefulMantBits) - 1) *
+            5**-fltFormat.log2OfMinSubnorm))
+
+        # Use %g mostly because it omits extra trailing zeros after the decimal
+        # point, and that's what I want. (Since I'm using such a large
+        # precision, simple values would look completely nuts otherwise.) The
+        # heuristic %g uses for fixed vs. exponential notation is reasonable;
+        # I might prefer exponential for large-magnitude values over a few tens
+        # of digits (say 1e+100), but I don't care enough to implement
+        # something myself.
+        exactString = "{val:.{prec}g}".format(val=value, prec=2*mostDigits)
+
+        # Check bound
+        sigFigs = exactString.partition("e")[0]
+        numSigFigs = len(sigFigs)
+        if "." in sigFigs:
+            numSigFigs -= 1
+        assert numSigFigs <= mostDigits
+
+        # Actually print it
+        print("Dec (exact):  {}".format(exactString))
+    else:
+        # The C standard says that this is the number of base-10 digits
+        # required to serialize all floating-point values of a given precision
+        # to decimal and recover the original values unambiguously. I don't
+        # have the background to prove this myself, but I'll trust the standard
+        # committee.
+        prec = int(math.ceil(1 +
+            (1 + fltFormat.usefulMantBits)*math.log(2, 10)))
+        print("Dec (approx): {val:.{prec}g}".format(val=value, prec=prec))
 
 def splitSEM(value, fltFormat):
     # TODO:
@@ -82,6 +144,9 @@ class FloatFormat(object):
         self.mantBits = mantBits
         self.explicitLeadingBit = explicitLeadingBit
 
+        # TODO shorter name?
+        # Or make it 3 values: storedMantBits, totalMantBits, trailingMantBits,
+        # so I'm forced to actually specify each time?
         self.usefulMantBits = mantBits
         if explicitLeadingBit:
             self.usefulMantBits -= 1
